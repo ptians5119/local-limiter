239d298bf22b0cfd1f8cfac29b1b0ac0b42f31a1
peter <pengte@teamgaga.com>
2025-09-15 11:05:52 +0800

    feat: 复原key的生成

--- src/local.rs
+++ src/local.rs
@@ -1,8 +1,10 @@
+use chrono::Timelike;
 use moka::future::Cache;
 use once_cell::sync::Lazy;
 use tokio::sync::Mutex;
 use std::time::Duration;
 
+// todo capacity的默认值
 pub static LOCAL_CACHE: Lazy<Mutex<Cache<String, i32>>> = Lazy::new(|| {
     let cache = Cache::builder()
         .time_to_live(Duration::from_secs(60))
@@ -13,9 +15,8 @@ pub static LOCAL_CACHE: Lazy<Mutex<Cache<String, i32>>> = Lazy::new(|| {
 });
 
 pub async fn count_by_user(id: i64) -> i32 {
-    // let sec = chrono::Utc::now().second();
-    // let key = format!("{}{}", id, sec);
-    let key = id.to_string();
+    let sec = chrono::Utc::now().second();
+    let key = format!("{}{}", id, sec);
     let local_cache = LOCAL_CACHE.lock().await;
     let user_cache = local_cache.entry(key.clone()).or_insert(0).await;
 
